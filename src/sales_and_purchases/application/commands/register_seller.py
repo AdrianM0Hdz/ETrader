664e32281f0ef4739fcd310e7749b5cfa2fee26a
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-07-27 10:18:17 -0600

    register seller now uses observer

--- src/sales_and_purchases/application/commands/register_seller.py
+++ src/sales_and_purchases/application/commands/register_seller.py
@@ -1,3 +1,6 @@
+from src.shared_kernel.application.observer.event_manager import EventManager
+from src.shared_kernel.domain.events.seller_created import SellerCreated
+
 from src.sales_and_purchases.domain.common.values import Name
 from src.sales_and_purchases.domain.common.values import Description
 
@@ -6,11 +9,17 @@ from src.sales_and_purchases.domain.seller.repository import SellerRepository
 
 
 class RegisterSeller:
-    def __init__(self, seller_repository: SellerRepository):
+    def __init__(
+        self, seller_repository: SellerRepository, event_manager: EventManager
+    ):
         self.seller_repository = seller_repository
+        self.event_manager = event_manager
 
     def __call__(self, name: str, description: str):
         seller = Seller.create_new(
             name=Name(name), description=Description(description)
         )
         self.seller_repository.add(seller)
+        self.event_manager.notify(
+            SellerCreated, SellerCreated.create_new(seller_id=seller.id.value)
+        )
